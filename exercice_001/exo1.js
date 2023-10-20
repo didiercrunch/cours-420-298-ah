187f2f581d65069cb8cf2792896b79777bfcb6a4
didier amyot <didier.amyot@gmail.com>
2023-10-20 19:24:52 -0400

    ss

--- exercice_001/exo1.js
+++ exercice_001/exo1.js
@@ -4,3 +4,18 @@ function exo1b(){
     let userInput = document.getElementById("input-1").value;
     document.getElementById("result-box-1").innerText = userInput;
 }
+
+function getInputEqualityMessage(input1, input2){
+    if(input1 === input2){
+        return "input-1 = input-2";
+    }
+    return "input-1 != input-2";
+}
+
+function exo1c(){
+    let input1 = document.getElementById("input-1").value;
+    let input2 = document.getElementById("input-2").value;
+
+    document.getElementById("result-box-2").innerText = getInputEqualityMessage(input1, input2);
+
+}
