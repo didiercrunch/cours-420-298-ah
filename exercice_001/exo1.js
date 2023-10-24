9d9bb663d3db8ad5d3bc4288d7d208967a2e83ce
didier amyot <didier.amyot@gmail.com>
2023-10-24 08:35:54 -0400

    Added results for exo 1.

--- exercice_001/exo1.js
+++ exercice_001/exo1.js
@@ -17,5 +17,42 @@ function exo1c(){
     let input2 = document.getElementById("input-2").value;
 
     document.getElementById("result-box-2").innerText = getInputEqualityMessage(input1, input2);
+}
 
+function isNumber(str) {
+    return str !== "" && !isNaN(str);
+}
+
+function getIsNumberMessage(input) {
+    if(isNumber(input)){
+        return "Number!";
+    }
+    return "n'est pas un nombre";
+}
+
+function exo1d(){
+    let input1 = document.getElementById("input-3").value;
+
+    document.getElementById("result-box-3").innerText = getIsNumberMessage(input1);
+}
+
+function getSizeMessage(n) {
+    if(n < 1000){
+        return "plus plus petit que 1000";
+    }
+    return "plus plus grand que 1000";
+}
+
+function getSafeSizeMessage(input) {
+    if(!isNumber(input)){
+        return "n'est pas un nombre";
+    }
+    return getSizeMessage(parseInt(input));
+
+}
+
+function exo1e(){
+    let input1 = document.getElementById("input-4").value;
+
+    document.getElementById("result-box-4").innerText = getSafeSizeMessage(input1);
 }
